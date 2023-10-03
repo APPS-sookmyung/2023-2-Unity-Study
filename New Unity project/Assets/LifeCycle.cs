6e322aeeda8b113f7c539eaa171af1126dd8d1ae
ljinny <104649081+ljinny@users.noreply.github.com>
2023-10-04 03:06:58 +0900

    Feat: 키보드, 마우스 입력

--- New Unity project/Assets/LifeCycle.cs
+++ New Unity project/Assets/LifeCycle.cs
@@ -2,45 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript1 : MonoBehaviour
+public class LifeCycle : MonoBehaviour
 {
-    void Awake() //초기화 영역
+   
+    void Update()
     {
-        Debug.Log("플레이어 데이터가 준비되었습니다.");
-    }
+        if (Input.anyKeyDown) // 게임 내 입력을 관리하는 클래스 (anyKeyDown : 아무 입력을 최초로 받을 때 true)
+            Debug.Log("플레이어가 아무 키를 눌렀습니다.");
 
-    void OnEnable() // 활성화 (초기화와 물리엔진 사이)
-    {
-        Debug.Log("플레이어가 로그인했습니다.");
-    }
+        //if (Input.anyKey)
+        //Debug.Log("플레이어가 아무 키를 누르고 있습니다.");
 
-    void Start() //초기화 영역
-    {
-        Debug.Log("사냥 장비를 챙겼습니다.");
-    }
+        // key, Mouse : down, stay, up
 
-    void FixedUpdate() // 물리 연산 업데이트 : 고정된 실행주기 -> CPU 많이 사용
-    {
-        Debug.Log("이동~");
-    }
+        // GetKey : 키보드 버튼 입력을 받으면 true
+        if (Input.GetKeyDown(KeyCode.Return)) // Return : Enter
+            Debug.Log("아이템을 구입하였습니다.");
 
-    void Update() // 게임 로직 업데이트 : 환경에 따라 실행주기가 달라짐
-    {
-        Debug.Log("몬스터 사냥!!"); // 60프레임으로 실행
-    }
+        if (Input.GetKey(KeyCode.LeftArrow))
+            Debug.Log("왼쪽으로 이동 중.");
 
-    void LateUpdate() // 모든 업데이트 끝난 후 (카메라, 후처리)
-    {
-        Debug.Log("경험치 획득.");
-    }
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+            Debug.Log("오른쪽 이동을 멈추었습니다.");
 
-    void OnDisable() // 게임 오브젝트가 비활성화 되었을 때 (게임로직과 해체 사이)
-    {
-        Debug.Log("플레이어가 로그아웃했습니다.");
-    }
+        //GetMouse : 마우스 버튼 입력을 받으면 true
+        // 0 : 마우스 왼쪽 버튼
+        if (Input.GetMouseButtonDown(0))
+            Debug.Log("미사일 발사!");
 
-    void OnDestroy() // 해체 (게임 오브젝트가 삭제될 때)
-    {
-        Debug.Log("플레이어 데이터가 해제하였습니다.");
+        if (Input.GetMouseButton(0))
+            Debug.Log("미사일 모으는 중.....");
+
+        if (Input.GetMouseButtonUp(0))
+            Debug.Log("슈퍼 미사일 발사!!!");
+
+        //GetButton : Input 버튼 입력을 받으면 true
+        if (Input.GetButtonDown("Jump"))
+            Debug.Log("점프!");
+
+        if (Input.GetButton("Jump"))
+            Debug.Log("점프 모으는 중......");
+
+        if (Input.GetButtonUp("Jump"))
+            Debug.Log("슈퍼 점프!!!");
+
+        if (Input.GetButton("Horizontal")) 
+            Debug.Log("횡 이동 중......" + Input.GetAxisRaw("Horizontal")); //GetAxisRaw : 가중치 없이 왼쪽 -1, 오른쪽 1 반환
+
+        if (Input.GetButton("Vertical"))
+            Debug.Log("종 이동 중......" + Input.GetAxisRaw("Verticalz"));
     }
 }
