76cfb221e9dc60d0b5518a15c16d96e015797393
mxinseo <jade201913@gmail.com>
2023-10-04 22:43:09 +0900

    [Feat] B10 - B12 강의 실습

---
+++ 경민서/2주차 실습/Otherball.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Otherball : MonoBehaviour
+{
+    MeshRenderer mesh;
+    Material mat;
+
+    void Start()
+    {
+        mesh = GetComponent<MeshRenderer>();
+        mat = mesh.material;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "MyBall")
+            mat.color = new Color(0, 0, 0);   
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.name == "MyBall")
+            mat.color = new Color(1, 1, 1);
+    }
+
+
+}
