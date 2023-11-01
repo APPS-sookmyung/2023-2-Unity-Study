be078228f04f1a018e110f05c837eb9d7dbd71de
ljinny <104649081+ljinny@users.noreply.github.com>
2023-11-02 04:14:02 +0900

    Feat : B13~B15 강의 실습

---
+++ 이지은/3주차/PlayerMove.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float maxSpeed;
+    Rigidbody2D rigid;
+    SpriteRenderer spriterenderer;
+    Animator anim;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriterenderer = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        // Stop Speed
+        if (Input.GetButtonUp("Horizontal"))
+        {
+            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.3f, rigid.velocity.y);
+        }
+
+        //Direction Sprite
+        if (Input.GetButton("Horizontal"))
+        {
+            spriterenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
+        }
+
+        //Animation
+        if (Mathf.Abs(rigid.velocity.x) < 0.3)
+            anim.SetBool("isWalking", false);
+
+        else
+            anim.SetBool("isWalking", true);  
+    }
+
+    void FixedUpdate()
+    {
+        //Move By Key Control
+        float h = Input.GetAxisRaw("Horizontal");
+        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+
+        if (rigid.velocity.x > maxSpeed) //Right Max Spped
+            rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
+
+        else if (rigid.velocity.x < maxSpeed*(-1)) //Left Max Speed
+            rigid.velocity = new Vector2(maxSpeed * (-1), rigid.velocity.y);
+    }
+}
